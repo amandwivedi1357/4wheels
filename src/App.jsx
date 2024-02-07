e0f9dffe27cbe1db44e8bf341531f4582eb65243
Aman Dwivedi <amandwivedi71124.com>
2024-02-07 12:42:06 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 
+import { useState } from 'react';
 import './App.css'
 import AllRoutes from './Routes/AllRoutes'
 import ScrollToTop from './components/Login/Scroll'
+import Loading from './components/designs/Loading';
 
 
 
 function App() {
+  const [loaded, setLoaded] = useState(false);
 
+  // Simulating image loading (replace this with actual image loading logic)
+  setTimeout(() => {
+    setLoaded(true);
+  }, 3000); // Simulating a delay of 3 seconds
 
   return (
     <div className='App'>
-    <ScrollToTop/>
-       <AllRoutes/>
+      {!loaded ? (
+        <Loading/>
+      ):(
+        <>
+        <ScrollToTop/>
+        <AllRoutes/>
+        </>
+      )}
+   
     </div>
   )
 }
